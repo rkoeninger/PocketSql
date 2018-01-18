0d096517b0b1003a04e87d6c9d5c04f71a49a3ee
Robert Koeninger <rkoeninger@att.net>
2018-01-18 17:47:03 -0500

    Added C# solution

---
+++ PocketSql.Lib/ParseIt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace PocketSql
+{
+    public static class ParseIt
+    {
+        public static void Main(string sql)
+        {
+            var parser = new TSql140Parser(false);
+            var input = new StringReader(sql);
+            var fragments = parser.Parse(input, out var errors);
+
+            if (errors.Count > 0)
+            {
+                throw new Exception(string.Join("\r\n", errors));
+            }
+
+            foreach (var token in fragments.ScriptTokenStream.Where(x => x.TokenType != TSqlTokenType.WhiteSpace))
+            {
+                if (token.TokenType != TSqlTokenType.WhiteSpace
+                    && token.TokenType != TSqlTokenType.EndOfFile)
+                {
+                    Console.WriteLine($"{token.TokenType} @ ({token.Line}, {token.Column}): \"{token.Text}\"");
+                }
+            }
+        }
+    }
+}
---
+++ PocketSql.Tests/ParsingTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PocketSql.Tests
+{
+    public class ParsingTests
+    {
+        [Test]
+        public void Test()
+        {
+            ParseIt.Main(@"
+                select *
+                from People
+                where Age > 30");
+        }
+    }
+}
