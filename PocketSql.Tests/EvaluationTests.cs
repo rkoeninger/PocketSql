da4a28f9557994a206636350305615d85caf2c37
Robert Koeninger <rkoeninger@att.net>
2018-01-23 13:40:23 -0500

    Expanded Person insert, select, update tests

--- PocketSql.Tests/EvaluationTests.cs
+++ PocketSql.Tests/EvaluationTests.cs
@@ -14,14 +14,28 @@ namespace PocketSql.Tests
 
             using (var connection = engine.GetConnection())
             {
-                Assert.AreEqual(-1, connection.Execute("create table People ( Name varchar(32), Age int )"));
+                connection.Execute("create table People ( Name varchar(32), Age int )");
 
                 Assert.AreEqual(1, connection.Execute("insert into People (Name, Age) values ('Rob', 30)"));
 
-                var people = connection.Query("select Name from People").ToList();
+                var people = connection.Query<Person>("select Name, Age from People").ToList();
                 Assert.AreEqual(1, people.Count);
                 Assert.AreEqual("Rob", people[0].Name);
+                Assert.AreEqual(30, people[0].Age);
+
+                Assert.AreEqual(1, connection.Execute("update People set Age += 1 where Name = 'Rob'"));
+
+                people = connection.Query<Person>("select Name, Age from People where Name = 'Rob'").ToList();
+                Assert.AreEqual(1, people.Count);
+                Assert.AreEqual("Rob", people[0].Name);
+                Assert.AreEqual(31, people[0].Age);
             }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
         }
     }
 }
