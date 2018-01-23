6f9755021d6bcefabd9c1f35c5d4056eaa7df5b3
Robert Koeninger <rkoeninger@att.net>
2018-01-22 22:49:54 -0500

    Used Dapper in tests

--- PocketSql.Tests/EvaluationTests.cs
+++ PocketSql.Tests/EvaluationTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Dapper;
+using NUnit.Framework;
 
 namespace PocketSql.Tests
 {
@@ -12,50 +14,13 @@ namespace PocketSql.Tests
 
             using (var connection = engine.GetConnection())
             {
-                var createCommand = connection.CreateCommand();
-                createCommand.CommandText = @"
-                    create table People
-                    (
-                        Name varchar(32),
-                        Age int
-                    )
-                ";
-                Assert.AreEqual(-1, createCommand.ExecuteNonQuery());
+                Assert.AreEqual(-1, connection.Execute("create table People ( Name varchar(32), Age int )"));
 
-                var insertCommand = connection.CreateCommand();
-                insertCommand.CommandText = @"
-                    insert into People
-                    (
-                        Name,
-                        Age
-                    )
-                    values
-                    (
-                        'Rob',
-                        30
-                    )
-                ";
-                Assert.AreEqual(1, insertCommand.ExecuteNonQuery());
+                Assert.AreEqual(1, connection.Execute("insert into People (Name, Age) values ('Rob', 30)"));
 
-                var selectCommand = connection.CreateCommand();
-                selectCommand.CommandText = @"
-                    select * from People
-                ";
-                var reader = selectCommand.ExecuteReader();
-                var totalRowCount = 0;
-
-                while (reader.NextResult())
-                {
-                    while (reader.Read())
-                    {
-                        totalRowCount++;
-                        Assert.AreEqual("Rob", reader.GetString(reader.GetOrdinal("Name")));
-                        Assert.AreEqual(30, reader.GetInt32(reader.GetOrdinal("Age")));
-                    }
-                }
-
-                reader.Close();
-                Assert.AreEqual(1, totalRowCount);
+                var people = connection.Query("select Name from People").ToList();
+                Assert.AreEqual(1, people.Count);
+                Assert.AreEqual("Rob", people[0].Name);
             }
         }
     }
