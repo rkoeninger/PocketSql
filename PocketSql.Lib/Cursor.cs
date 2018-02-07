e81886c5824cc95840d86b10d4b47f5be8628368
Robert Koeninger <rkoeninger@att.net>
2018-02-07 00:02:38 -0500

    Added Cursor class

---
+++ PocketSql.Lib/Cursor.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using PocketSql.Evaluation;
+
+namespace PocketSql
+{
+    public class Cursor
+    {
+        public Cursor(QueryExpression query)
+        {
+            this.query = query;
+        }
+
+        private readonly QueryExpression query;
+        private DataTable results;
+        private int index;
+
+        public void Open(Env env)
+        {
+            results = Eval.Evaluate(query, env).ResultSet;
+        }
+
+        public DataRow FetchNext(Env env)
+        {
+            if (index >= results.Rows.Count)
+            {
+                // env.FetchStatus = ERROR;
+                return null;
+            }
+
+            // env.FetchStatus = SUCCESS;
+            return results.Rows[index++];
+        }
+    }
+}
