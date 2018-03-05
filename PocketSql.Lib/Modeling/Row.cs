88aa0dfda3eb028774b714a75032f3239590f0e7
Robert Koeninger <rkoeninger@att.net>
2018-03-05 00:06:21 -0500

    Added GetColumnOrdinal(string[], Scope) to Table, Row

--- PocketSql.Lib/Modeling/Row.cs
+++ PocketSql.Lib/Modeling/Row.cs
@@ -12,9 +12,7 @@ namespace PocketSql.Modeling
 
         public Column GetColumn(int ordinal) => Columns[ordinal];
 
-        public Column GetColumn(string name) =>
-            Columns.FirstOrDefault(c => name != null && name.Similar(c.Name.LastOrDefault()))
-                ?? throw new Exception($"Column \"{name}\" does not exist in row");
+        public Column GetColumn(string name) => GetColumn(GetColumnOrdinal(name));
 
         public int GetColumnOrdinal(string name)
         {
@@ -23,6 +21,28 @@ namespace PocketSql.Modeling
                     return i;
 
             throw new Exception($"Column \"{name}\" does not exist in row");
+        }
+
+        // TODO: expand name with default database/schema/match tables in scope
+        public int GetColumnOrdinal(string[] name, Scope scope)
+        {
+            if (name.Length == 0) throw new Exception("Cannot access column without name");
+            if (name.Length == 1) return GetColumnOrdinal(name[0]);
+
+            var resolvedName = name
+                .Take(name.Length - 1)
+                .SelectMany(x =>
+                    scope.Aliases.TryGetValue(x, out var resolved)
+                        ? resolved
+                        : new[] { x })
+                .Concat(new[] { name.Last() })
+                .ToArray();
+
+            for (var i = 0; i < Columns.Count; ++i)
+                if (name != null && name.SequenceEqual(Columns[i].Name, Naming.Comparer))
+                    return i;
+
+            throw new Exception($"Column \"{string.Join(".", name)}\" does not exist in row");
         }
 
         public object GetValue(string name) =>
--- PocketSql.Lib/Modeling/Table.cs
+++ PocketSql.Lib/Modeling/Table.cs
@@ -23,9 +23,7 @@ namespace PocketSql.Modeling
 
         public Column GetColumn(int ordinal) => Columns[ordinal];
 
-        public Column GetColumn(string name) =>
-            Columns.FirstOrDefault(c => name != null && name.Similar(c.Name.LastOrDefault()))
-                ?? throw new Exception($"Column \"{name}\" does not exist in table \"{Name}\"");
+        public Column GetColumn(string name) => GetColumn(GetColumnOrdinal(name));
 
         public int GetColumnOrdinal(string name)
         {
@@ -34,6 +32,28 @@ namespace PocketSql.Modeling
                     return i;
 
             throw new Exception($"Column \"{name}\" does not exist in table \"{Name}\"");
+        }
+
+        // TODO: expand name with default database/schema/match tables in scope
+        public int GetColumnOrdinal(string[] name, Scope scope)
+        {
+            if (name.Length == 0) throw new Exception("Cannot access column without name");
+            if (name.Length == 1) return GetColumnOrdinal(name[0]);
+
+            var resolvedName = name
+                .Take(name.Length - 1)
+                .SelectMany(x =>
+                    scope.Aliases.TryGetValue(x, out var resolved)
+                        ? resolved
+                        : new[] { x })
+                .Concat(new[] { name.Last() })
+                .ToArray();
+
+            for (var i = 0; i < Columns.Count; ++i)
+                if (name != null && name.SequenceEqual(Columns[i].Name, Naming.Comparer))
+                    return i;
+
+            throw new Exception($"Column \"{string.Join(".", name)}\" does not exist in row");
         }
 
         public Table CopyLayout() =>
