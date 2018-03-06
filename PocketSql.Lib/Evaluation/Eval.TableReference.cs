0f37ba1dbaccfc39000520c010a09433065887f6
Robert Koeninger <rkoeninger@att.net>
2018-03-06 18:24:23 -0500

    Removed unneeded null check

--- PocketSql.Lib/Evaluation/Eval.TableReference.cs
+++ PocketSql.Lib/Evaluation/Eval.TableReference.cs
@@ -13,13 +13,15 @@ namespace PocketSql.Evaluation
                 case NamedTableReference named:
                     var baseName = named.SchemaObject.BaseIdentifier.Value;
                     var schema = scope.Env.GetSchema(named.SchemaObject);
-                    return (schema.Views.GetMaybe(baseName)
-                        .Select(v => Evaluate(v.Query, scope).ResultSet)
-                        .OrElse(() => schema.Tables[baseName]),
-                        named.Alias == null ? scope :
-                        scope.PushAlias(
-                            named.Alias.Value,
-                            named.SchemaObject.Identifiers.Select(x => x.Value).ToArray()));
+                    return (
+                        schema.Views.GetMaybe(baseName)
+                            .Select(v => Evaluate(v.Query, scope).ResultSet)
+                            .OrElse(() => schema.Tables[baseName]),
+                        named.Alias == null
+                            ? scope
+                            : scope.PushAlias(
+                                named.Alias.Value,
+                                named.SchemaObject.Identifiers.Select(x => x.Value).ToArray()));
                 case DataModificationTableReference dml:
                     // TODO: how does this work?
                     return (Evaluate(dml.DataModificationSpecification, scope).ResultSet, scope);
